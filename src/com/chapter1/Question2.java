d8cc6fb706a954b3e3f50bb88c91fcc965b5932e
Ravi <ravi.tej36@gmail.com>
2015-08-20 20:41:27 -0700

    branch1 commit

--- src/com/chapter1/Question2.java
+++ src/com/chapter1/Question2.java
@@ -1,9 +1,10 @@
-package com.chapter1
+package com.chapter1;
 
 public class Question2 {
 	
    public static void main(String [] args) {
-      // commit from master	
+      // commit from master
+	// this is 2nd line from master .
    }	
 	
 
