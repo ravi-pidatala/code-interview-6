331ec6c170133b41959f1bb07f6e859513c307f2
Ravi <ravi.tej36@gmail.com>
2015-08-22 23:30:13 -0700

    treenode

---
+++ src/com/chapter4/TreeNode.java
@@ -0,0 +1,24 @@
+package com.chapter4;
+
+
+public class TreeNode {
+
+	private TreeNode left;
+	private TreeNode right;
+
+	public TreeNode getLeft() {
+		return left;
+	}
+	
+	public void setLeft() {
+		this.left = left;
+	}
+
+	public TreeNode getRight() {
+		return right;
+	}
+	
+	public TreeNode setRight() {
+		this.right = right;
+	}
+}
